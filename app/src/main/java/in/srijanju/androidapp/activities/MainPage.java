2790cf7cc8b8c6cb2e00e8feac0cec5a80d08699
gauravllh03 <gaurav.llh03@gmail.com>
2020-01-23 21:02:20 +0530

    main page updated

--- app/src/main/java/in/srijanju/androidapp/activities/MainPage.java
+++ app/src/main/java/in/srijanju/androidapp/activities/MainPage.java
@@ -1,16 +1,60 @@
 package in.srijanju.androidapp.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import in.srijanju.androidapp.R;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainPage extends AppCompatActivity {
 
+	TextView a;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_page);
+		a=(TextView)findViewById(R.id.timer);
+
+
+		final Calendar thatDay = Calendar.getInstance();
+		thatDay.setTime(new Date(0)); /* reset */
+		thatDay.set(Calendar.DAY_OF_MONTH,5);
+		thatDay.set(Calendar.MONTH,2); // 0-11 so 1 less
+		thatDay.set(Calendar.YEAR, 2020);
+
+		final int SECONDS_IN_A_DAY = 24 * 60 * 60;
+		Timer t = new Timer();
+		t.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				Calendar today = Calendar.getInstance();
+				long diff =  thatDay.getTimeInMillis() - today.getTimeInMillis();
+				long diffSec = diff / 1000;
+
+				long days = diffSec / SECONDS_IN_A_DAY;
+				long secondsDay = diffSec % SECONDS_IN_A_DAY;
+				long seconds = secondsDay % 60;
+				long minutes = (secondsDay / 60) % 60;
+				long hours = (secondsDay / 3600); // % 24 not needed
+
+				String s=days+" DAYS " + hours +" HOURS "+minutes+ " MINUTES " +seconds+" SECONDSto go!";
+				setText(a,s);
+
+			}
+		}, 0, 1000);
+
+	}
+	private void setText(final TextView text,final String value){
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				text.setText(value);
+			}
+		});
 	}
 }
