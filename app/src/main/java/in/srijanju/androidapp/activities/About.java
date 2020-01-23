6c48a05daf57ae7dd7e3cd58311864dea56c0918
Rishav Agarwal <rishavcoder@gmail.com>
2020-01-23 21:39:26 +0530

    fix: main page buttons

---
+++ app/src/main/java/in/srijanju/androidapp/activities/About.java
@@ -0,0 +1,16 @@
+package in.srijanju.androidapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import in.srijanju.androidapp.R;
+
+public class About extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+}
--- app/src/main/java/in/srijanju/androidapp/activities/MainPage.java
+++ app/src/main/java/in/srijanju/androidapp/activities/MainPage.java
@@ -1,30 +1,38 @@
 package in.srijanju.androidapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import in.srijanju.androidapp.R;
-import java.util.Date;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import in.srijanju.androidapp.R;
+
 public class MainPage extends AppCompatActivity {
 
-	TextView a;
+	private TextView a;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_page);
-		a=(TextView)findViewById(R.id.timer);
 
+		initialise();
 
+		/*
+		 * Srijan timer logic
+		 * */
 		final Calendar thatDay = Calendar.getInstance();
 		thatDay.setTime(new Date(0)); /* reset */
-		thatDay.set(Calendar.DAY_OF_MONTH,5);
-		thatDay.set(Calendar.MONTH,2); // 0-11 so 1 less
+		thatDay.set(Calendar.DAY_OF_MONTH, 5);
+		thatDay.set(Calendar.MONTH, 2); // 0-11 so 1 less
 		thatDay.set(Calendar.YEAR, 2020);
 
 		final int SECONDS_IN_A_DAY = 24 * 60 * 60;
@@ -33,7 +41,7 @@ public class MainPage extends AppCompatActivity {
 			@Override
 			public void run() {
 				Calendar today = Calendar.getInstance();
-				long diff =  thatDay.getTimeInMillis() - today.getTimeInMillis();
+				long diff = thatDay.getTimeInMillis() - today.getTimeInMillis();
 				long diffSec = diff / 1000;
 
 				long days = diffSec / SECONDS_IN_A_DAY;
@@ -42,14 +50,28 @@ public class MainPage extends AppCompatActivity {
 				long minutes = (secondsDay / 60) % 60;
 				long hours = (secondsDay / 3600); // % 24 not needed
 
-				String s=days+" DAYS " + hours +" HOURS "+minutes+ " MINUTES " +seconds+" SECONDSto go!";
-				setText(a,s);
+				String s = days + " DAYS " + hours + " HOURS " + minutes + " MINUTES " + seconds + " " +
+					"SECONDSto go!";
+				setText(a, s);
 
 			}
 		}, 0, 1000);
 
 	}
-	private void setText(final TextView text,final String value){
+
+	private void initialise() {
+		a = findViewById(R.id.timer);
+
+		ImageView ivAbout = findViewById(R.id.iv_about);
+		ivAbout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainPage.this, About.class));
+			}
+		});
+	}
+
+	private void setText(final TextView text, final String value) {
 		runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
