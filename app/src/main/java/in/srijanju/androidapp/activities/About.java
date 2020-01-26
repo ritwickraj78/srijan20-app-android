837ff1518272a6fe4a2b8608b717aa9b4c2b8545
gauravllh03 <gaurav.llh03@gmail.com>
2020-01-27 03:05:03 +0530

    event_description_styling

--- app/src/main/java/in/srijanju/androidapp/activities/About.java
+++ app/src/main/java/in/srijanju/androidapp/activities/About.java
@@ -2,6 +2,14 @@ package in.srijanju.androidapp.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -112,6 +120,7 @@ public class About extends YouTubeBaseActivity implements YouTubePlayer.OnInitia
 			this.context = context;
 			this.values = values;
 		}
+
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
