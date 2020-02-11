a96dee53bba5df7a16699b05a42f3a9ecb4482be
Rishav Agarwal <rishavcoder@gmail.com>
2020-02-11 13:02:08 +0530

    fix: sign in

--- app/src/main/java/in/srijanju/androidapp/SrijanApp.java
+++ app/src/main/java/in/srijanju/androidapp/SrijanApp.java
@@ -1,8 +1,12 @@
 package in.srijanju.androidapp;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -21,6 +25,43 @@ public class SrijanApp extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+			@Override
+			public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+				activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+			}
+
+			@Override
+			public void onActivityStarted(@NonNull Activity activity) {
+
+			}
+
+			@Override
+			public void onActivityResumed(@NonNull Activity activity) {
+
+			}
+
+			@Override
+			public void onActivityPaused(@NonNull Activity activity) {
+
+			}
+
+			@Override
+			public void onActivityStopped(@NonNull Activity activity) {
+
+			}
+
+			@Override
+			public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+			}
+
+			@Override
+			public void onActivityDestroyed(@NonNull Activity activity) {
+
+			}
+		});
 
 		// Firebase realtime database caching
 		FirebaseDatabase db = FirebaseDatabase.getInstance();
