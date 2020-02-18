200333a6998177d8e6aafceb89d08056b468fbc4
RitwickRaj78 <ritwickraj78@gmail.com>
2020-02-18 23:17:35 +0530

    Added Navigation Drawer

--- app/src/main/java/in/srijanju/androidapp/view/MainPage.java
+++ app/src/main/java/in/srijanju/androidapp/view/MainPage.java
@@ -3,12 +3,22 @@ package in.srijanju.androidapp.view;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,11 +30,20 @@ import java.util.TimerTask;
 import in.srijanju.androidapp.R;
 import in.srijanju.androidapp.SrijanActivity;
 
-public class MainPage extends SrijanActivity {
+public class MainPage extends SrijanActivity implements
+		NavigationView.OnNavigationItemSelectedListener {
   public static final String SRIJAN_MERCH_URL = "https://www.srijanju.in/app/merchandise";
   public static final String SRIJAN_WORKSHOP_URL = "https://www.srijanju.in/app/workshops";
 
   private TextView a;
+
+	public Toolbar toolbar;
+
+	public DrawerLayout drawerLayout;
+
+	public NavController navController;
+
+	public NavigationView navigationView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -42,140 +61,205 @@ public class MainPage extends SrijanActivity {
 	  finish();
 	  return;
 	}
+	setupNavigation();
+//	initialise();
+//
+//	/*
+//	 * Srijan timer logic
+//	 * */
+//	final Calendar thatDay = Calendar.getInstance();
+//	thatDay.setTime(new Date(0)); /* reset */
+//	thatDay.set(Calendar.DAY_OF_MONTH, 5);
+//	thatDay.set(Calendar.MONTH, 2); // 0-11 so 1 less
+//	thatDay.set(Calendar.YEAR, 2020);
+//	thatDay.set(Calendar.HOUR_OF_DAY, 0);
+//
+//	final Calendar lastDay = Calendar.getInstance();
+//	lastDay.setTime(new Date(0)); /* reset */
+//	lastDay.set(Calendar.DAY_OF_MONTH, 9);
+//	lastDay.set(Calendar.MONTH, 2); // 0-11 so 1 less
+//	lastDay.set(Calendar.YEAR, 2020);
+//	thatDay.set(Calendar.HOUR_OF_DAY, 0);
+//
+//	final int SECONDS_IN_A_DAY = 24 * 60 * 60;
+//	Timer t = new Timer();
+//	t.schedule(new TimerTask() {
+//	  @Override
+//	  public void run() {
+//		Calendar today = Calendar.getInstance();
+//		long diff = thatDay.getTimeInMillis() - today.getTimeInMillis();
+//		if (diff >= 0) {
+//		  long diffSec = diff / 1000;
+//
+//		  long days = diffSec / SECONDS_IN_A_DAY;
+//		  long secondsDay = diffSec % SECONDS_IN_A_DAY;
+//		  // long seconds = secondsDay % 60;
+//		  // long minutes = (secondsDay / 60) % 60;
+//		  long hours = (secondsDay / 3600); // % 24 not needed
+//
+//		  String d = days > 1 ? " DAYS \n" : " DAY \n";
+//		  String h = hours > 1 ? " HOURS \n to go! " : " HOUR \n to go! ";
+//		  String s = days + d + hours + h;
+//		  setText(a, s);
+//		} else {
+//		  long difference = lastDay.getTimeInMillis() - today.getTimeInMillis();
+//		  // long diffSec = difference / 1000;
+//		  if (difference >= 0) {
+//			// long days = diffSec / SECONDS_IN_A_DAY;
+//			// long secondsDay = diffSec % SECONDS_IN_A_DAY;
+//			// long seconds = secondsDay % 60;
+//			// long minutes = (secondsDay / 60) % 60;
+//			// long hours = (secondsDay / 3600); // % 24 not needed
+//			String s = " Srijan \n is live \n now";
+//			setText(a, s);
+//		  } else {
+//			setText(a, " Srijan\n over");
+//		  }
+//		}
+//
+//	  }
+//	}, 500, 1000 * 60 * 5);
+  }
+	private void setupNavigation() {
 
-	initialise();
+		toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-	/*
-	 * Srijan timer logic
-	 * */
-	final Calendar thatDay = Calendar.getInstance();
-	thatDay.setTime(new Date(0)); /* reset */
-	thatDay.set(Calendar.DAY_OF_MONTH, 5);
-	thatDay.set(Calendar.MONTH, 2); // 0-11 so 1 less
-	thatDay.set(Calendar.YEAR, 2020);
-	thatDay.set(Calendar.HOUR_OF_DAY, 0);
+		drawerLayout = findViewById(R.id.drawer_layout);
 
-	final Calendar lastDay = Calendar.getInstance();
-	lastDay.setTime(new Date(0)); /* reset */
-	lastDay.set(Calendar.DAY_OF_MONTH, 9);
-	lastDay.set(Calendar.MONTH, 2); // 0-11 so 1 less
-	lastDay.set(Calendar.YEAR, 2020);
-	thatDay.set(Calendar.HOUR_OF_DAY, 0);
+		navigationView = findViewById(R.id.navigationView);
 
-	final int SECONDS_IN_A_DAY = 24 * 60 * 60;
-	Timer t = new Timer();
-	t.schedule(new TimerTask() {
-	  @Override
-	  public void run() {
-		Calendar today = Calendar.getInstance();
-		long diff = thatDay.getTimeInMillis() - today.getTimeInMillis();
-		if (diff >= 0) {
-		  long diffSec = diff / 1000;
+		navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
-		  long days = diffSec / SECONDS_IN_A_DAY;
-		  long secondsDay = diffSec % SECONDS_IN_A_DAY;
-		  // long seconds = secondsDay % 60;
-		  // long minutes = (secondsDay / 60) % 60;
-		  long hours = (secondsDay / 3600); // % 24 not needed
+		NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
 
-		  String d = days > 1 ? " DAYS \n" : " DAY \n";
-		  String h = hours > 1 ? " HOURS \n to go! " : " HOUR \n to go! ";
-		  String s = days + d + hours + h;
-		  setText(a, s);
+		NavigationUI.setupWithNavController(navigationView, navController);
+
+		navigationView.setNavigationItemSelectedListener(this);
+
+	}
+
+//  private void initialise() {
+//	a = findViewById(R.id.timer);
+//
+//	ImageView ivProfile = findViewById(R.id.iv_profile);
+//	ivProfile.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, Profile.class));
+//	  }
+//	});
+//
+//	ImageView ivAbout = findViewById(R.id.iv_about);
+//	ivAbout.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, About.class));
+//	  }
+//	});
+//
+//	ImageView ivEvents = findViewById(R.id.iv_events);
+//	ivEvents.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, Events.class));
+//	  }
+//	});
+//
+//	ImageView ambassador = findViewById(R.id.iv_ca);
+//	ambassador.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, Ambassador.class));
+//	  }
+//	});
+//
+//	ImageView gallery = findViewById(R.id.iv_gallery);
+//	gallery.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, Gallery.class));
+//	  }
+//	});
+//
+//	ImageView ivSponsor = findViewById(R.id.iv_sponsor);
+//	ivSponsor.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, Sponsors.class));
+//	  }
+//	});
+//
+//	ImageView ivScanQr = findViewById(R.id.iv_scan_qr);
+//	ivScanQr.setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		startActivity(new Intent(MainPage.this, CameraScan.class));
+//	  }
+//	});
+//
+//	findViewById(R.id.iv_merch).setOnClickListener(new View.OnClickListener() {
+//	  @Override
+//	  public void onClick(View v) {
+//		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(SRIJAN_MERCH_URL));
+//		startActivity(browserIntent);
+//	  }
+//	});
+//  }
+//
+//  private void setText(final TextView text, final String value) {
+//	runOnUiThread(new Runnable() {
+//	  @Override
+//	  public void run() {
+//		text.setText(value);
+//	  }
+//	});
+//  }
+
+
+
+	@Override
+	public boolean onSupportNavigateUp() {
+		return NavigationUI.navigateUp(drawerLayout, Navigation.findNavController(this, R.id.nav_host_fragment));
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+			drawerLayout.closeDrawer(GravityCompat.START);
 		} else {
-		  long difference = lastDay.getTimeInMillis() - today.getTimeInMillis();
-		  // long diffSec = difference / 1000;
-		  if (difference >= 0) {
-			// long days = diffSec / SECONDS_IN_A_DAY;
-			// long secondsDay = diffSec % SECONDS_IN_A_DAY;
-			// long seconds = secondsDay % 60;
-			// long minutes = (secondsDay / 60) % 60;
-			// long hours = (secondsDay / 3600); // % 24 not needed
-			String s = " Srijan \n is live \n now";
-			setText(a, s);
-		  } else {
-			setText(a, " Srijan\n over");
-		  }
+			super.onBackPressed();
 		}
+	}
 
-	  }
-	}, 500, 1000 * 60 * 5);
-  }
+	@Override
+	public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-  private void initialise() {
-	a = findViewById(R.id.timer);
+		menuItem.setChecked(true);
 
-	ImageView ivProfile = findViewById(R.id.iv_profile);
-	ivProfile.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, Profile.class));
-	  }
-	});
+		drawerLayout.closeDrawers();
 
-	ImageView ivAbout = findViewById(R.id.iv_about);
-	ivAbout.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, About.class));
-	  }
-	});
+		int id = menuItem.getItemId();
 
-	ImageView ivEvents = findViewById(R.id.iv_events);
-	ivEvents.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, Events.class));
-	  }
-	});
+		switch (id) {
 
-	ImageView ambassador = findViewById(R.id.iv_ca);
-	ambassador.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, Ambassador.class));
-	  }
-	});
+			case R.id.first:
+				navController.navigate(R.id.firstFragment);
+				break;
 
-	ImageView gallery = findViewById(R.id.iv_gallery);
-	gallery.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, Gallery.class));
-	  }
-	});
+			case R.id.second:
+				navController.navigate(R.id.secondFragment);
+				break;
 
-	ImageView ivSponsor = findViewById(R.id.iv_sponsor);
-	ivSponsor.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, Sponsors.class));
-	  }
-	});
+			case R.id.third:
+				navController.navigate(R.id.thirdFragment);
+				break;
 
-	ImageView ivScanQr = findViewById(R.id.iv_scan_qr);
-	ivScanQr.setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		startActivity(new Intent(MainPage.this, CameraScan.class));
-	  }
-	});
+		}
+		return true;
 
-	findViewById(R.id.iv_merch).setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(SRIJAN_MERCH_URL));
-		startActivity(browserIntent);
-	  }
-	});
-  }
+	}
 
-  private void setText(final TextView text, final String value) {
-	runOnUiThread(new Runnable() {
-	  @Override
-	  public void run() {
-		text.setText(value);
-	  }
-	});
-  }
 }
--- app/src/main/java/in/srijanju/androidapp/view/Profile.java
+++ app/src/main/java/in/srijanju/androidapp/view/Profile.java
@@ -1,17 +1,18 @@
 package in.srijanju.androidapp.view;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
-
+import androidx.fragment.app.Fragment;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,128 +22,114 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
-
 import in.srijanju.androidapp.R;
 import in.srijanju.androidapp.controller.EventRegisteredAdapter;
 import in.srijanju.androidapp.model.User;
-
-public class Profile extends AppCompatActivity {
-
-  final ArrayList<Pair<String, String>> events = new ArrayList<>();
-  final EventRegisteredAdapter adapter = new EventRegisteredAdapter(this, events);
-  FirebaseUser user;
-  ChildEventListener regEventListener = new ChildEventListener() {
-	@Override
-	public void onChildAdded(
-			@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-	  events.add(Pair.create(String.valueOf(dataSnapshot.child("event").getValue()),
-			  String.valueOf(dataSnapshot.child("team").getValue())));
-	  adapter.notifyDataSetChanged();
-	}
-
-	@Override
-	public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-	}
-
-	@Override
-	public void onChildRemoved(
-			@NonNull DataSnapshot dataSnapshot) {
-	  events.remove(Pair.create(dataSnapshot.child("event").toString(),
-			  dataSnapshot.child("team").toString()));
-	  adapter.notifyDataSetChanged();
-	}
-
-	@Override
-	public void onChildMoved(
-			@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-	}
-
-	@Override
-	public void onCancelled(
-			@NonNull DatabaseError databaseError) {
-	}
-  };
-
-  DatabaseReference ref;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_profile);
-
-	user = FirebaseAuth.getInstance().getCurrentUser();
-	if (user == null) {
-	  Toast.makeText(this, "Not logged in", Toast.LENGTH_SHORT).show();
-	  FirebaseAuth.getInstance().signOut();
-	  AuthUI.getInstance().signOut(getApplicationContext());
-	  Intent intent = new Intent(Profile.this, MainActivity.class);
-	  intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-	  startActivity(intent);
-	  finish();
-	  return;
-	}
-
-	final TextView tvName = findViewById(R.id.tv_profile_name);
-	final TextView tvEmail = findViewById(R.id.tv_profile_email);
-	final TextView tvClg = findViewById(R.id.tv_clg);
-	final TextView tvDegree = findViewById(R.id.tv_degree);
-	final TextView tvCourse = findViewById(R.id.tv_course);
-	final TextView tvYear = findViewById(R.id.tv_year);
-
-	FirebaseDatabase.getInstance().getReference("srijan/profile/" + user.getUid() +
-			"/parentprofile").addListenerForSingleValueEvent(new ValueEventListener() {
-	  @Override
-	  public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-		User user = dataSnapshot.getValue(User.class);
-		if (user == null) {
-		  Toast.makeText(Profile.this, "Something weird happened! :(", Toast.LENGTH_SHORT).show();
-		  finish();
-		  return;
+public class Profile extends Fragment {
+	final ArrayList<Pair<String, String>> events = new ArrayList<>();
+	final EventRegisteredAdapter adapter = new EventRegisteredAdapter(getActivity(), events);
+	FirebaseUser user;
+	ChildEventListener regEventListener = new ChildEventListener() {
+		@Override
+		public void onChildAdded(
+				@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+			events.add(Pair.create(String.valueOf(dataSnapshot.child("event").getValue()),
+					String.valueOf(dataSnapshot.child("team").getValue())));
+			adapter.notifyDataSetChanged();
 		}
-		tvName.setText(user.name);
-		tvEmail.setText(user.email);
-		tvClg.setText(user.college);
-		tvDegree.setText(user.degree);
-		tvCourse.setText(String.format("Department of %s", user.course));
-		tvYear.setText(String.format("%s year", user.year));
-	  }
-
-	  @Override
-	  public void onCancelled(@NonNull DatabaseError databaseError) {
-	  }
-	});
-
-	FirebaseDatabase db = FirebaseDatabase.getInstance();
-	ref = db.getReference("srijan/profile/" + user.getUid() + "/events");
-
-	GridView gridView = findViewById(R.id.gv_events);
-	gridView.setAdapter(adapter);
-
-	findViewById(R.id.btn_profile_logout).setOnClickListener(new View.OnClickListener() {
-	  @Override
-	  public void onClick(View v) {
-		FirebaseAuth.getInstance().signOut();
-		AuthUI.getInstance().signOut(getApplicationContext());
-		Intent intent = new Intent(Profile.this, MainActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-		startActivity(intent);
-		finish();
-	  }
-	});
-  }
-
-  @Override
-  protected void onResume() {
-	super.onResume();
-	ref.addChildEventListener(regEventListener);
-  }
-
-  @Override
-  protected void onPause() {
-	super.onPause();
-	ref.removeEventListener(regEventListener);
-	events.clear();
-  }
+		@Override
+		public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+		}
+		@Override
+		public void onChildRemoved(
+				@NonNull DataSnapshot dataSnapshot) {
+			events.remove(Pair.create(dataSnapshot.child("event").toString(),
+					dataSnapshot.child("team").toString()));
+			adapter.notifyDataSetChanged();
+		}
+		@Override
+		public void onChildMoved(
+				@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+		}
+		@Override
+		public void onCancelled(
+				@NonNull DatabaseError databaseError) {
+		}
+	};
+	DatabaseReference ref;
+	@Override
+	public void onAttach(Context context) {
+		super.onAttach(context);
+	}
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.activity_profile, container, false);
+	}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		user = FirebaseAuth.getInstance().getCurrentUser();
+		if (user == null) {
+			Toast.makeText(getActivity(), "Not logged in", Toast.LENGTH_SHORT).show();
+			FirebaseAuth.getInstance().signOut();
+			AuthUI.getInstance().signOut(getContext());
+			Intent intent = new Intent(getActivity(), MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+			startActivity(intent);
+			return;
+		}
+		final TextView tvName = getView().findViewById(R.id.tv_profile_name);
+		final TextView tvEmail = getView().findViewById(R.id.tv_profile_email);
+		final TextView tvClg = getView().findViewById(R.id.tv_clg);
+		final TextView tvDegree = getView().findViewById(R.id.tv_degree);
+		final TextView tvCourse = getView().findViewById(R.id.tv_course);
+		final TextView tvYear = getView().findViewById(R.id.tv_year);
+		FirebaseDatabase.getInstance().getReference("srijan/profile/" + user.getUid() +
+				"/parentprofile").addListenerForSingleValueEvent(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+				User user = dataSnapshot.getValue(User.class);
+				if (user == null) {
+					Toast.makeText(getActivity(), "Something weird happened! :(", Toast.LENGTH_SHORT).show();
+					return;
+				}
+				tvName.setText(user.name);
+				tvEmail.setText(user.email);
+				tvClg.setText(user.college);
+				tvDegree.setText(user.degree);
+				tvCourse.setText(String.format("Department of %s", user.course));
+				tvYear.setText(String.format("%s year", user.year));
+			}
+			@Override
+			public void onCancelled(@NonNull DatabaseError databaseError) {
+			}
+		});
+		FirebaseDatabase db = FirebaseDatabase.getInstance();
+		ref = db.getReference("srijan/profile/" + user.getUid() + "/events");
+		GridView gridView = getView().findViewById(R.id.gv_events);
+		gridView.setAdapter(adapter);
+		getView().findViewById(R.id.btn_profile_logout).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				FirebaseAuth.getInstance().signOut();
+				AuthUI.getInstance().signOut(getActivity().getApplicationContext());
+				Intent intent = new Intent(getActivity(), MainActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+				startActivity(intent);
+			}
+		});
+	}
+	@Override
+	public void onResume() {
+		super.onResume();
+		ref.addChildEventListener(regEventListener);
+	}
+	@Override
+	public void onPause() {
+		super.onPause();
+		ref.removeEventListener(regEventListener);
+		events.clear();
+	}
 }
